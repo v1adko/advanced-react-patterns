63fd3662e7f2ab977b157ea5143d4f0b696f1b90
Vlad Kolbaya <vlad.kolbaya@gmail.com>
2017-12-08 23:53:33 +0200

    10 - Render Prop

--- src/render-props-10/App.js
+++ src/render-props-10/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Toggle from './Toggle';
+import Switch from './Switch';
 
 function App() {
   return (
     <Toggle
       onToggle={on => console.log('toggle', on)}
+      render={({ on, toggle }) => (
+        <div>
+          <Switch on={on} onClick={toggle} />
+          {on ? 'on' : 'off'}
+        </div>
+      )}
     />
-  )
+  );
 }
 
 export default App;
--- src/render-props-10/Toggle.js
+++ src/render-props-10/Toggle.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import Switch from './Switch';
 
 class Toggle extends React.Component {
-  static defaultProps = {onToggle: () => {}}
-  state = {on: false}
+  static defaultProps = { onToggle: () => {} };
+  state = { on: false };
   toggle = () =>
     this.setState(
-      ({on}) => ({on: !on}),
+      ({ on }) => ({ on: !on }),
       () => {
-        this.props.onToggle(this.state.on)
-      },
-    )
+        this.props.onToggle(this.state.on);
+      }
+    );
+
   render() {
-    const {on} = this.state
-    return (
-      <Switch on={on} onClick={this.toggle} />
-    )
+    return this.props.render({
+      on: this.state.on,
+      toggle: this.toggle
+    });
   }
 }
 
