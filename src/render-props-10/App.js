55df193e339875495224239491d6d4f86bb04895
Vlad Kolbaya <vlad.kolbaya@gmail.com>
2017-12-09 17:52:20 +0200

    Prepare for the next part

--- src/render-props-10/App.js
+++ src/render-props-10/App.js
@@ -1,35 +1,81 @@
 import React from 'react';
 import Toggle from './Toggle';
 import Switch from './Switch';
+import MyUpperCaseInput from './MyUpperCaseInput';
 
-function App() {
-  return (
-    <Toggle
-      defaultOn={true}
-      onToggle={on => console.log('toggle', on)}
-      onReset={on => console.log('reset', on)}
-      render={toggle => (
-        <div>
-          <Switch
-            {...toggle.getTogglerProps({
-              on: toggle.on
-            })}
-          />
-          {toggle.on ? 'on' : 'off'}
-          <hr />
-          <button
-            {...toggle.getTogglerProps({
-              onClick: () => alert('hi')
-            })}
-          >
+class App extends React.Component {
+  initialState = { timesClicked: 0, on: false };
+  state = this.initialState;
+
+  handleToggle = () => {
+    this.setState(({ timesClicked, on }) => ({
+      thimesClicked: timesClicked + 1,
+      on: timesClicked >= 4 ? false : !on
+    }));
+  };
+
+  handleReset;
+
+  render() {
+    const { timesClicked, on } = this.state;
+    return [
+      <Toggle
+        key={'render-props'}
+        defaultOn={true}
+        onToggle={on => console.log('toggle', on)}
+        onReset={on => console.log('reset', on)}
+        render={toggle => (
+          <div>
+            <Switch
+              {...toggle.getTogglerProps({
+                on: toggle.on
+              })}
+            />
             {toggle.on ? 'on' : 'off'}
-          </button>
-          <hr />
-          <button onClick={() => toggle.reset()}>Reset</button>
-        </div>
-      )}
-    />
-  );
+            <br />
+            <button
+              {...toggle.getTogglerProps({
+                onClick: () => alert('hi')
+              })}
+            >
+              {toggle.on ? 'on' : 'off'}
+            </button>
+            <br />
+            <button onClick={() => toggle.reset()}>Reset</button>
+          </div>
+        )}
+      />,
+      <hr key="separator-1" />,
+      /* 14 Controlled component -example */
+      <MyUpperCaseInput key="controlled-component-example" />,
+      <hr key="separator-2" />,
+      /* 14 Control props in contro */
+      <Toggle
+        key="controlled-component-props"
+        on={on}
+        onToggle={this.handleToggle}
+        onReset={this.handleReset}
+        render={toggle => (
+          <div>
+            <Switch
+              {...toggle.getTogglerProps({
+                on: toggle.on
+              })}
+            />
+            {timesClicked > 4 ? (
+              <div>
+                Whoa, you've clicked too much!
+                <br />
+                <button onClick={toggle.reset}>reset</button>
+              </div>
+            ) : timesClicked > 0 ? (
+              <div>Click count: {timesClicked}</div>
+            ) : null}
+          </div>
+        )}
+      />
+    ];
+  }
 }
 
 export default App;
---
+++ src/render-props-10/MyUpperCaseInput.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class MyUpperCaseInput extends React.Component {
+  state = { upperValue: '', lowerValue: '' };
+
+  handleChange = e =>
+    this.setState({
+      upperValue: e.target.value.toUpperCase(),
+      lowerValue: e.target.value.toLowerCase()
+    });
+
+  render() {
+    const { upperValue, lowerValue } = this.state;
+    return (
+      <div>
+        <label>
+          Upper:
+          <input value={upperValue} onChange={this.handleChange} />
+        </label>
+        <br />
+        <label>
+          Lower:
+          <input value={lowerValue} onChange={this.handleChange} />
+        </label>
+      </div>
+    );
+  }
+}
+
+export default MyUpperCaseInput;
